71b46c380b86dd03cff723fa75a49ec456a2c27f
kurtis <152489765+jordikurtis@users.noreply.github.com>
2026-04-20 11:51:02 +0200

    Add files via upload

---
+++ TennecoOPCUA/Program.cs
@@ -0,0 +1,264 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Opc.Ua;
+using Opc.Ua.Client;
+using Opc.Ua.Configuration;
+
+namespace TennecoOPCUA
+{
+   static  class Program
+    {
+        static async Task Main()
+        {
+            // Remplacer avec la vraie IP quand disponible 
+            const string serverUrl = "opc.tcp://BRSC-FGFXSQ3.pt.int.tenneco.com:53530/OPCUA/SimulationServer";
+            //"opc.tcp://192.168.0.2:4840";
+            // "opc.tcp://BRSC-FGFXSQ3.pt.int.tenneco.com:53530/OPCUA/SimulationServer"; // 10.14.67.11 
+            // var endpointUrl = "opc.tcp://192.168.0.2:4840";
+
+            try
+            {
+                // STEP 1 — Configuration
+                const string certStore =
+                    @"%CommonApplicationData%\OPC Foundation\CertificateStores";
+
+                /*
+                 * La création d'une nouvelle configuration d'application consiste 
+                 * à définir tous les paramètres nécessaires qui définissent 
+                 * - le comportement, 
+                 - la communication et 
+                 - la sécurisation des connexions de l'application cliente avec le serveur OPC UA. 
+                 Ceci est une étape fondamentale lors de l'utilisation de la bibliothèque standard .NET de l'OPC Foundation
+                 -creation d'une instance de la Application . 
+                (ici est un peu different car la variable config prend tous en parametre qui ensuite ira dans  ^^ApplicationInstance ...   )
+                - ces deux methode  " ApplicationInstance " ET  " ApplicationConfiguration " s'occupe du App setup  coté client.
+                */
+
+                var config = new ApplicationConfiguration
+                {
+                    ApplicationName = "OPC UA - JORDI-TENNECO ",
+
+                    /*
+                     * c'est un identifiant universel (URI) unique au monde pour votre instance d'application OPC UA.
+                     * -> Son rôle principal est de distinguer cette instance d'application de toute autre application ou instance du réseau.
+                     */
+                    //Format alternative :-  ApplicationUri = $"urn:{Utils.GetHostName()}:OpcUaClientExample",
+
+                    ApplicationUri = Utils.Format(
+                                        @"urn:{0}:OPC UA - JORDI-TENNECO",
+                                        System.Net.Dns.GetHostName()),
+                    ApplicationType = ApplicationType.Client,
+
+                    ClientConfiguration = new ClientConfiguration
+                    {
+                        DefaultSessionTimeout = 60000
+                    },
+                    TransportQuotas = new TransportQuotas
+                    {
+                        OperationTimeout = 15000
+
+                        /*
+                         IL Y'A d'autres varibale comme 
+                        - MaxStringLength (aussi pour serveur)-1048576
+                        - MaxByteStringLength 1048576
+                        -MaxArrayLength (aussi pour serveur) 65535
+                        -MaxMessageSize (aussi pour serveur) 4194304
+                        -MaxBufferSize (aussi pour serveur) 65535
+                        -channelLifetime (aussi pour serveur) 300000
+                        SecurityTokenLifetime (aussi pour serveur) 3600000
+                         */
+                    },
+                    /* alternative mais avoir un fichier PKI dans le dossier du fichier 
+                   SecurityConfiguration = new SecurityConfiguration
+            {
+                ApplicationCertificate = new CertificateIdentifier
+                {
+                    StoreType = CertificateStoreType.Directory,
+                    StorePath = Path.Combine("pki", "own"),
+                    SubjectName = "CN=OpcUaClientExample"
+                },
+                TrustedPeerCertificates = new CertificateTrustList
+                {
+                    StoreType = CertificateStoreType.Directory,
+                    StorePath = Path.Combine("pki", "trusted")
+                },
+                RejectedCertificateStore = new CertificateTrustList
+                {
+                    StoreType = CertificateStoreType.Directory,
+                    StorePath = Path.Combine("pki", "rejected")
+                },
+                AutoAcceptUntrustedCertificates = true,
+                RejectSHA1SignedCertificates = false
+            },
+
+            TransportConfigurations = new TransportConfigurationCollection(),
+            TransportQuotas = new TransportQuotas { OperationTimeout = 15000 },
+            ClientConfiguration = new ClientConfiguration { DefaultSessionTimeout = 60000 }
+        };
+                     */
+
+                    SecurityConfiguration = new SecurityConfiguration
+                    {
+                        ApplicationCertificate = new CertificateIdentifier
+                        {// OÚ le certicate de L'instance de l'application est stocké !
+                            StoreType = "Directory",
+                            StorePath = $"{certStore}\\MachineDefault",
+                            SubjectName = "OPC UA - JORDI-TENNECO"
+                        },
+                        TrustedPeerCertificates = new CertificateTrustList
+                        { // oú les certificats de confiance sont stocké !
+                            StoreType = "Directory",
+                            StorePath = $"{certStore}\\UA Applications"
+                        }, // oú la perssone qui donne le certificat stocke le certificat !
+                        TrustedIssuerCertificates = new CertificateTrustList
+                        { 
+                            StoreType = "Directory",
+                            StorePath = $"{certStore}\\UA Certificate Authorities"
+                        },
+                        RejectedCertificateStore = new CertificateTrustList
+                        {
+                            StoreType = "Directory",
+                            StorePath = $"{certStore}\\RejectedCertificates"
+                        },
+                        AutoAcceptUntrustedCertificates = true,
+                        AddAppCertToTrustedStore = true
+                    }
+                };
+
+                // STEP 2 — Validation + Certificat
+
+                // API MODERNE — CheckApplicationInstanceCertificate supprimée
+                await config.ValidateAsync(ApplicationType.Client); // meilleur que -->   await config.Validate (.... ) ; 
+                // verifie si le certificat est OK ! 
+
+                /*
+                 ceci est une inline event handler ecrite en fonction Lamdba
+                s ->  refers to the "sender" — the object raising the event , dans ce cas << CertificateValidator.>>
+                e -> is an instance of event arguments, usually derived from EventArgs. 
+                In certificate validation, it might include information like the certificate being 
+                validated and whether validation succeeded.
+                 */
+                config.CertificateValidator.CertificateValidation += (s, e) =>
+                {
+                    e.Accept = true;
+                };
+
+                // STEP 3 — Endpoint
+                Console.WriteLine($"Essaie de ce connecter au Module : {serverUrl}");
+
+                //  Signature corrigée
+                var endpointDesc = CoreClientUtils.SelectEndpoint(config,serverUrl, useSecurity: false);
+                var endpointConfig = EndpointConfiguration.Create(config);
+                var endpoint = new ConfiguredEndpoint(null, endpointDesc, endpointConfig);
+
+                // STEP 4 — Creation de la Session
+                using var session = await Session.Create(
+                     config,
+                    endpoint,
+                    false, // la variable est - updateBeforeConnect :
+                    "OpcUaPoCSession", // la variabale est sessionName : 
+                    60000, // La variable est sessionTimeout : 
+                    new UserIdentity(new AnonymousIdentityToken()),
+                    null); // la variabale est preferredLocales : 
+
+                Console.WriteLine(" Connected!");
+                Console.WriteLine($"   Session ID : {session.SessionId}");
+                Console.WriteLine($"   Server URI : {session.Endpoint.Server.ApplicationUri}");
+
+                // STEP 5 — Subscription + Live Monitoring
+                var subscription = CreateSubscription(session);
+                AddMonitoredItems(subscription);
+
+                Console.WriteLine("\nAppui sur n'importe Quelle touche sur le Clavier Pour arréter ... !!!");
+                Console.ReadKey();
+
+
+                session.CloseAsync(); // meilleur que --> session.Close() ; 
+                Console.WriteLine(" Déconnecter .");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"\n Une Erreur est intervenu ; l'Erreur est    : {ex.Message}");
+                Console.WriteLine($" voici les details ;  Details : {ex.StackTrace}");
+            }
+        }
+
+        
+        // ==== SUBSCRIPTION + LIVE MONITORING OPC UA ====
+        
+
+        private static Subscription CreateSubscription(Session session)
+        {
+            var subscription = new Subscription(session.DefaultSubscription)
+            {
+                PublishingInterval = 3000,   // Apres combien de temps le serveur envoie les données (changeable )
+                                             // ici toutes les 3s
+                PublishingEnabled = true,
+                Priority = 0
+            };
+
+            session.AddSubscription(subscription);
+            subscription.Create();
+
+            Console.WriteLine("Subscription créée pour une récupération Live des données ");
+
+            return subscription;
+        }
+
+        private static void AddMonitoredItems(Subscription subscription)
+        {
+            var nodesToMonitor = new List<string>
+    {
+       // "ns=3;i=1007",
+        "ns=3;i=1001",
+        "ns=3;i=1002",
+        //"ns=3;i=1003",
+        "ns=3;i=1004",
+       // "ns=3;i=1005", 
+        "ns=3;i=1006" 
+        // ...... ajoute ici autant de NodeId que tu veux
+
+    };
+
+            foreach (var node in nodesToMonitor)
+            {
+                var monitoredItem = new MonitoredItem(subscription.DefaultItem)
+                {
+                   // StartNodeId = new NodeId(node),
+                   StartNodeId = NodeId.Parse(node), // cette approach est plus robust , l'autre marche mais pourrais crash !! 
+                    AttributeId = Attributes.Value,
+                    SamplingInterval = 0,    // Quand tu met 0 ca veux dire -> envoie les données aussi rapide que possible
+                    QueueSize = 10,
+                    DiscardOldest = true
+                };
+
+                monitoredItem.Notification += OnDataChanged;
+                subscription.AddItem(monitoredItem);
+
+                Console.WriteLine($" Monitoring actif des Noeuds  : {node}");
+            }
+
+            subscription.ApplyChanges();
+        }
+
+        private static void OnDataChanged(
+            MonitoredItem item,
+            MonitoredItemNotificationEventArgs e)
+        {
+            foreach (var value in item.DequeueValues())
+            {
+                Console.WriteLine("--- LIVE UPDATE DES NOEUDS ---");
+                Console.WriteLine($"NodeId    : {item.StartNodeId}");
+                Console.WriteLine($"Type      :  { value.Value?.GetType().Name}");
+                Console.WriteLine($"Valeur    : {value.Value}");
+                Console.WriteLine($"Status    : {value.StatusCode}");
+                Console.WriteLine($"Timestamp : {value.SourceTimestamp}");
+            }
+        }
+    }
+}
+
+
