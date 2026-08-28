e41751fadb72906ff0c234ba6502844db373d725
kurtis <152489765+jordikurtis@users.noreply.github.com>
2026-08-28 16:15:09 +0200

    Add files via upload
    ASPN.API

---
+++ OPCUA_PROJECT.Api/Models/MachineGroupe.cs
@@ -0,0 +1,9 @@
+﻿namespace OPCUA_PROJECT.Api.Models
+{
+    public class MachineGroupe
+    {
+ public int Id { get; set; }
+public string Name { get; set; } = "";
+public string Description { get; set; } = "";
+    }
+}
---
+++ OPCUA_PROJECT.Api/Repositories/IMachineGroupRepository.cs
@@ -0,0 +1,9 @@
+﻿using OPCUA_PROJECT.Api.Models;
+namespace OPCUA_PROJECT.Api.Repositories
+{
+    public class IMachineGroupRepository
+    {
+        Task<IEnumerable<MachineGroup>> GetAllAsync();
+
+    }
+}
