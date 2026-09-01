e81099d6a1a5f14b1cb8456691ea9154688ddf64
kurtis <152489765+jordikurtis@users.noreply.github.com>
2026-09-01 15:59:23 +0200

    Delete OPCUA_PROJECT.Api directory

--- OPCUA_PROJECT.Api/Models/MachineGroupe.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace OPCUA_PROJECT.Api.Models
-{
-    public class MachineGroupe
-    {
- public int Id { get; set; }
-public string Name { get; set; } = "";
-public string Description { get; set; } = "";
-    }
-}
--- OPCUA_PROJECT.Api/Repositories/IMachineGroupRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using OPCUA_PROJECT.Api.Models;
-namespace OPCUA_PROJECT.Api.Repositories
-{
-    public class IMachineGroupRepository
-    {
-        Task<IEnumerable<MachineGroup>> GetAllAsync();
-
-    }
-}
